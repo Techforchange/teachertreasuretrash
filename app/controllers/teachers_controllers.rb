8081d2a8adbe2b58ef6f65f83cad7e339e5f7265
Techforchange <arleneperezsb@gmail.com>
2016-12-01 12:44:25 -0800

    Creates bcrypt password for new teacher registrant

--- app/controllers/teachers_controllers.rb
+++ app/controllers/teachers_controllers.rb
@@ -1,3 +1,42 @@
+
 get '/teachers/new' do
   erb :'/teachers/new'
 end
+
+post '/teachers' do
+  @teacher = Teacher.new(params[:teacher])
+  if @teacher.save
+    session[:id] = @teacher.id
+    redirect "/teachers/#{@teacher.id}"
+  else
+    # @errors = @teacher.errors.full_messages
+    erb :'teachers/new'
+  end
+end
+
+get '/teachers/:id' do
+  @teacher = Teacher.find(params[:id])
+  erb :'teachers/show'
+end
+
+# USERS EDIT
+# get '/users/:id/edit' do
+#   @user = User.find(params[:id])
+#   erb :'users/edit'
+# end
+#
+#
+# # USERS UPDATE
+# put '/users/:id' do
+#   @user = User.find(params[:id])
+#   @user.update(params[:user])
+#   redirect "/users/#{@user.id}"
+# end
+#
+#
+# # USERS DESTROY
+# delete '/users/:id' do
+#   @user = User.find(params[:id])
+#   @user.destroy
+#   redirect "/users"
+# end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,24 @@
 class Teacher < ActiveRecord::Base
   has_many :resources, foreign_key: :giver_id
   has_many :resources, foreign_key: :receiver_id
-  # Remember to create a migration!
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  validates_presence_of :password
+  validates_presence_of :email
+  validates_uniqueness_of :email
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def authenticate?(email, txt_password)
+    self.email == email && self.password == txt_password
+  end
+
 end
