57ab46a5e199d2cf4c462065269410c65dc456f8
Techforchange <arleneperezsb@gmail.com>
2016-12-01 18:16:33 -0800

    Adds the new resource form under a specific category

--- app/controllers/resources_controllers.rb
+++ app/controllers/resources_controllers.rb
@@ -1,21 +1,36 @@
-get 'categories/:category_id/resources' do
+get '/categories/:category_id/resources' do
   @category = Category.find(params[:category_id])
   @resources = @category.resources
    redirect :"/categories/#{category.id}"
 end
 
-get '/resources/:id' do
-  #one specific resource
+get '/categories/:category_id/resources/new' do
+  @category = Category.find(params[:category_id])
+  erb :'/resources/new'
 end
 
-post '/resources' do
+post '/categories/:category_id/resources' do
+  @category = Category.find(params[:category_id])
+
   #adds new resource
 end
 
-get '/resources/:id/edit' do
+get '/categories/:category_id/resources/:id' do
+  @category = Category.find(params[:category_id])
+  @resources = @category.resources
+  #one specific resource
+end
+
+put '/categories/:category_id/resources/edit' do
+  @category = Category.find(params[:category_id])
+  @resources = @category.resources
   #edits a resource
 end
 
-delete '/resources/:id' do
+delete '/categories/:category_id/resources/:id' do
+  @category = Category.find(params[:category_id])
+  @resources = @category.resources.find(params[:id])
+  @resources.destroy
   #delete resource
+  #set giver id to user who clicked(current user) on link
 end
